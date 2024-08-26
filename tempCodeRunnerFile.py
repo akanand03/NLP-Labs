1f6cee39ca2f222347aaf9dd52e5c320522c1e55
Akshit Anand <94638561+akanand03@users.noreply.github.com>
2024-08-26 22:53:10 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+the requirements",
+    "designing the software": "architecting the solution",
+    "coding": "writing the program",
+    "developers": "coders",
+    "programming languages": "coding languages",
+    "tools": "instruments",
+    "good software": "effective applications",
+    "writing code": "scripting"
+}
