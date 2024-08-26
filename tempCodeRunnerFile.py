aabe3bbf5150040ead4d50ee1f39739608291c6a
Akshit Anand <94638561+akanand03@users.noreply.github.com>
2024-08-26 22:58:38 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-the requirements",
-    "designing the software": "architecting the solution",
-    "coding": "writing the program",
-    "developers": "coders",
-    "programming languages": "coding languages",
-    "tools": "instruments",
-    "good software": "effective applications",
-    "writing code": "scripting"
-}
